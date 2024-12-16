9fa3396c9854d6a2e8133f1dabe4637df990ff28
namodaj <nenad@amodaj.com>
2024-12-15 21:45:31 -0800

    v 2.0.9

--- mmstudio/src/main/java/org/micromanager/internal/MMVersion.java
+++ mmstudio/src/main/java/org/micromanager/internal/MMVersion.java
@@ -33,5 +33,5 @@ package org.micromanager.internal;
  * generate (overwrite) MMVersion.java.
  */
 public class MMVersion {
-   public static final String VERSION_STRING = "2.0.8";
+   public static final String VERSION_STRING = "2.0.9";
 }
